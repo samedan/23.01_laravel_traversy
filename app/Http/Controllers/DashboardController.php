7c42c2cb4faa1f5eef39b935c43c8ac1b1c1913c
Daniel Popescu <same.dan@gmail.com>
2023-01-12 16:13:52 +0100

    Posts added

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function __construct() {
+        // construct uses the middleware for all the controller
+        $this->middleware(['auth']);
+    }
+    public function index() {
+        return view('dashboard');
+    } 
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\auth\RegisterController;
+
+
+// Home
+Route::get('/', function() {
+    return view('home');
+})->name('home');
+// Dashboard
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
+// Logout
+Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
+// Login
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
+// Register
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
+// Posts
+Route::get('/posts', [PostController::class, 'index'])->name('posts');
+Route::post('/posts', [PostController::class, 'store']);
+
