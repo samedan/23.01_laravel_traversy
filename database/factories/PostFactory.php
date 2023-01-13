4394b5642c548f223f402d9f1256c38cf37fb798
Daniel Popescu <same.dan@gmail.com>
2023-01-13 16:38:02 +0100

    Like/Unlike HTML

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -8,7 +8,9 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
  */
 class PostFactory extends Factory
+
 {
+    // protected $model = Post::class;
     /**
      * Define the model's default state.
      *
@@ -17,7 +19,7 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'body' => fake()->sentence(20)
         ];
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -35,6 +35,14 @@
         <div class="mb-4">
           <a href="" class="font-bold">{{ $post->user->username }}</a> <span class="text-grat-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
           <p class="mb-2">{{$post->body }}</p>
+          <div class="flex items-center">
+            <form action="" method="post" class="mr-1">
+              <button type="submit" class="text-blue-500">Like</button>
+            </form>
+            <form action="" method="post" class="mr-1">
+              <button type="submit" class="text-blue-500">Unlike</button>
+            </form>
+          </div>
         </div>
       @endforeach
 {{-- Pagination --}}
